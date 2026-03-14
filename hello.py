42bb010d3e49c13dfe62b7109aec8858e6c64329
Enes Ozd <enes.ozdemir93@gmail.com>
2026-03-14 16:40:03 +0300

    Day01: expand hello script with age and city

---
+++ hello.py
@@ -0,0 +1,8 @@
+name = input("Adınız= ")
+age = input("Yaşınız= ")
+city = input("Şehriniz= ")
+
+print("\n----Bilgilerin----")
+print("\nMerhaba, ",name)
+print("\nYaşın=",age)
+print("\nŞehrin=",city)
